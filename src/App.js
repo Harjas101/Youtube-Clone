9028fc0cda9499d14670b019bfb79f25deed9b2e
Harjas101 <anand.harjas@gmail.com>
2022-11-15 19:36:14 -0500

    added component structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import Navbar from './components/Navbar';
+import Feed from './components/Feed';
+import VideoDetail from './components/VideoDetail';
+import SearchFeed from './components/SearchFeed';
+import ChannelDetail from './components/ChannelDetail';
 
-const App = () => {
-  return (
-    <div>APP
-      hello this is my app
-    </div>
+const App = () => 
+  (
+    <BrowserRouter>
+    <Box sx={{backgroundColor: '#000'}}>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Feed/>} />
+          <Route path='/video/:id' element={<VideoDetail />}/>
+          <Route path='/Search/:id' element={<SearchFeed />}/>
+          <Route path='/channel/:id' element={<ChannelDetail />}/>
+        </Routes>
+    </Box>
+    </BrowserRouter>
   )
-}
+
 
 export default App
