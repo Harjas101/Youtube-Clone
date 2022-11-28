227a7243cb272a6414f2bfd75abd220f01dfe1e9
Harjas101 <anand.harjas@gmail.com>
2022-11-28 15:23:32 -0500

    debugging, fixed api fetch calls

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,16 +1,17 @@
 import axios from "axios";
-const BASE_URL = "https://youtube-v31.p.rapidapi.com/";
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 const options = {
   params: {
     maxResults: 50,
   },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+    'X-RapidAPI-Key': '0a541a7215mshcf5407821f35232p1d7358jsn1173d3a79f29',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   },
 };
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  
   return data;
 };
